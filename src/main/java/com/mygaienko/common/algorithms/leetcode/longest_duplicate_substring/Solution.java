e8632a5ac3d27d0df26c9db2a56a22c0983580d0
Dmytro Mygaenko <dmygaenko@gmail.com>
2021-10-31 00:17:13 +0300

    longest_duplicate_substring

--- src/main/java/com/mygaienko/common/algorithms/leetcode/longest_duplicate_substring/Solution.java
+++ src/main/java/com/mygaienko/common/algorithms/leetcode/longest_duplicate_substring/Solution.java
@@ -1,13 +1,32 @@
 package com.mygaienko.common.algorithms.leetcode.longest_duplicate_substring;
 
 
+import java.util.*;
+
 /**
  * https://leetcode.com/problems/longest-duplicate-substring/
  */
 public class Solution {
 
     public String longestDupSubstring(String s) {
-        return "";
+
+        Map<String, Integer> sortedSubstrings = new TreeMap<>(Comparator
+                .comparingInt(String::length).reversed()
+                .thenComparing(String::hashCode));
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j <= s.length(); j++) {
+                String substring = s.substring(i, j);
+                sortedSubstrings.compute(substring, (k, occur) -> occur == null ? 1 : occur + 1);
+            }
+        }
+
+        return sortedSubstrings.entrySet()
+                .stream()
+                .filter(entry -> entry.getValue() > 1)
+                .findFirst()
+                .map(Map.Entry::getKey)
+                .orElse("");
     }
 
 }
