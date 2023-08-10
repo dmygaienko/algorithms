408e9fe1e272629f587c78c930780b084b6c0e1c
dmytromygaienko <dmygaenko@gmail.com>
2023-08-10 23:08:51 +0300

    snapshot_array with inverted binary search

--- src/main/java/com/mygaienko/common/algorithms/leetcode/snapshot_array/SnapshotArray.java
+++ src/main/java/com/mygaienko/common/algorithms/leetcode/snapshot_array/SnapshotArray.java
@@ -1,35 +1,35 @@
-package com.mygaienko.common.algorithms.leetcode.snapshot_array;
+ package com.mygaienko.common.algorithms.leetcode.snapshot_array;
 
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.TreeMap;
 
 class SnapshotArray {
 
-    int SNAP_OPT = 5;
+    int SNAP_OPT = 10000;
     int snap_id = 0;
     Map<Integer, int[]> snapshots = new HashMap<>();
-    Map<Integer, Map<Integer, Integer>> compactSnapshots = new HashMap<>();
+    Map<Integer, TreeMap<Integer, Integer>> compactSnapshots = new HashMap<>();
 
     public SnapshotArray(int length) {
         snapshots.put(snap_id, new int[length]);
     }
 
     public void set(int index, int val) {
-       if (isOriginal(snap_id)) {
-           snapshots.get(snap_id)[index] = val;
-       } else {
-           compactSnapshots.get(snap_id).put(index, val);
-       }
+        if (isOriginal(snap_id)) {
+            snapshots.get(snap_id)[index] = val;
+        } else {
+            compactSnapshots.putIfAbsent(index, new TreeMap<>());
+            compactSnapshots.get(index).put(snap_id, val);
+        }
     }
 
     public int snap() {
         ++snap_id;
         if (isOriginal(snap_id)) {
             buildSnapshot();
-        } else {
-            compactSnapshots.put(snap_id, new HashMap<>());
         }
         return snap_id - 1;
     }
@@ -42,29 +42,14 @@ class SnapshotArray {
         if (isOriginal(snap_id)) {
             return snapshots.get(snap_id)[index];
         } else {
-            int right = snap_id;
-            int left = snap_id - snap_id % SNAP_OPT;
+            int left = snap_id - (snap_id % SNAP_OPT);
 
-            Integer result = null;
-            while (left <= right) {
-                int mid = left + (right - left) / 2;
+            TreeMap<Integer, Integer> snapshotsValues = compactSnapshots.get(index);
+            if (snapshotsValues == null
+                    || snapshotsValues.isEmpty()
+                    || snapshotsValues.floorEntry(snap_id) == null) return snapshots.get(left)[index];
 
-                Integer lastVal;
-                if (!isOriginal(mid)) {
-                    lastVal = compactSnapshots.get(mid).get(index);
-                } else {
-                    return snapshots.get(mid)[index];
-                }
-
-                if (lastVal != null) {
-                    result = lastVal;
-                    left = mid;
-                } else {
-                    right = mid - 1;
-                }
-            }
-
-            return result;
+            return snapshotsValues.floorEntry(snap_id).getValue();
         }
     }
 
@@ -74,13 +59,14 @@ class SnapshotArray {
         int[] newArray = Arrays.copyOf(prevOriginalArray, prevOriginalArray.length);
         snapshots.put(snap_id, newArray);
 
-        int localSnapId = prevSnapId + 1;
-        while (localSnapId != snap_id) {
-            Map<Integer, Integer> compactSnapshot = compactSnapshots.get(localSnapId);
-            for (Entry<Integer, Integer> entry : compactSnapshot.entrySet()) {
-                newArray[entry.getKey()] = entry.getValue();
+        for (int i = 0; i < newArray.length; i++) {
+            TreeMap<Integer, Integer> compactSnapshot = compactSnapshots.get(i);
+            if (compactSnapshot != null && !compactSnapshot.isEmpty()) {
+                Entry<Integer, Integer> entry = compactSnapshot.lastEntry();
+                if (entry.getValue() >= prevSnapId) {
+                    newArray[i] = entry.getValue();
+                }
             }
-            localSnapId++;
         }
     }
 
--- src/test/java/com/mygaienko/common/algorithms/leetcode/snapshot_array/SnapshotArrayTest.java
+++ src/test/java/com/mygaienko/common/algorithms/leetcode/snapshot_array/SnapshotArrayTest.java
@@ -25,4 +25,60 @@ public class SnapshotArrayTest {
         snapshotArray.snap();
         assertThat(snapshotArray.get(0,0)).isEqualTo(15);
     }
+
+    /**
+     * ["SnapshotArray","snap","snap","set","snap","get","set","get","snap","get"]
+     * [[1],            [],     [],   [0,4], [],  [0,1],[0,12],[0,1],[],[0,3]]
+     */
+    @Test
+    public void test1() {
+        SnapshotArray snapshotArray = new SnapshotArray(1);
+        snapshotArray.snap();
+        snapshotArray.snap();
+        snapshotArray.set(0, 4);
+        snapshotArray.snap();
+        assertThat(snapshotArray.get(0,1)).isEqualTo(4);
+        snapshotArray.set(0, 12);
+        assertThat(snapshotArray.get(0,1)).isEqualTo(4);
+        snapshotArray.snap();
+        assertThat(snapshotArray.get(0,3)).isEqualTo(12);
+    }
+
+    /**
+     * ["SnapshotArray","set","snap","snap","set","set","get","get","get"]
+     * [[3],            [1,6],[],    [],    [1,19],[0,4],[2,1],[2,0],[0,1]]
+     */
+    @Test
+    public void test3() {
+        SnapshotArray snapshotArray = new SnapshotArray(3);
+        snapshotArray.set(1, 6);
+        snapshotArray.snap();
+        snapshotArray.snap();
+        snapshotArray.set(1, 19);
+        snapshotArray.set(0, 4);
+        assertThat(snapshotArray.get(2,1)).isEqualTo(0);
+        assertThat(snapshotArray.get(2,0)).isEqualTo(0);
+        assertThat(snapshotArray.get(0,1)).isEqualTo(0);
+    }
+
+    /**
+     * ["SnapshotArray","set",  "set","snap","get", "set","snap","set","set","get","get"]
+     * [[3],            [1,18], [1,4],[]    ,[0,0], [0,20],[],   [0,2],[1,1],[1,1],[1,0]]
+     */
+    @Test
+    public void test57() {
+        SnapshotArray snapshotArray = new SnapshotArray(3);
+        snapshotArray.set(1, 18);
+        snapshotArray.set(1, 4);
+        snapshotArray.snap();
+        snapshotArray.snap();
+        assertThat(snapshotArray.get(0,0)).isEqualTo(0);
+
+        snapshotArray.set(0, 20);
+        snapshotArray.snap();
+        snapshotArray.set(0, 2);
+        snapshotArray.set(1, 1);
+        assertThat(snapshotArray.get(1,1)).isEqualTo(4);
+        assertThat(snapshotArray.get(1,0)).isEqualTo(4);
+    }
 }
