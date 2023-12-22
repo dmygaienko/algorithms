cc570cf82799129f1cbdc5c48587b2c4ec185868
dmygaienko <dmygaienko@playtika.com>
2023-12-23 00:08:08 +0200

    countNicePairs

--- src/main/java/com/mygaienko/common/algorithms/leetcode/count_nice_pairs_in_an_array/Solution.java
+++ src/main/java/com/mygaienko/common/algorithms/leetcode/count_nice_pairs_in_an_array/Solution.java
@@ -3,7 +3,7 @@ package com.mygaienko.common.algorithms.leetcode.count_nice_pairs_in_an_array;
 class Solution {
 
     public int countNicePairs(int[] nums) {
-        return 0;
+        return 1;
     }
 
 }
